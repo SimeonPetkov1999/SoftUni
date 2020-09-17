72b10ff8e53830f573fb1a30754432b97c03ff15
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-17 17:12:50 +0300

    Basic Syntax Homework

---
+++ C# Fundamentals/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/BasicSyntaxConditionalStatementsAndLoops_Exercise/03.Vacation/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            string typeOfGroup = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double price = 0;
+
+            switch (dayOfWeek)
+            {
+                case "Friday":
+                    switch (typeOfGroup)
+                    {
+                        case"Students":
+                            price = 8.45;
+                            break;
+                        case "Business":
+                            price = 10.90;
+                            break;
+                        case "Regular":
+                            price = 15;
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                    switch (typeOfGroup)
+                    {
+                        case "Students":
+                            price = 9.80;
+                            break;
+                        case "Business":
+                            price = 15.60;
+                            break;
+                        case "Regular":
+                            price = 20;
+                            break;
+                    }
+                    break;
+                case "Sunday":
+                    switch (typeOfGroup)
+                    {
+                        case "Students":
+                            price = 10.46;
+                            break;
+                        case "Business":
+                            price = 16;
+                            break;
+                        case "Regular":
+                            price = 22.50;
+                            break;
+                    }
+                    break;
+            }
+
+            if (typeOfGroup=="Business" && numberOfPeople>=100)
+            {
+                numberOfPeople -= 10;
+            }
+
+            double totalPrice = numberOfPeople * price;
+
+            if (typeOfGroup=="Students" && numberOfPeople>=30)
+            {
+                totalPrice *= 0.85;
+            }
+
+            else if (typeOfGroup=="Regular"&& (numberOfPeople>=10&&numberOfPeople<=20))
+            {
+                totalPrice *= 0.95;
+            }
+            Console.WriteLine($"Total price: {totalPrice:f2}");
+        }
+    }
+}
---
+++ C# Fundamentals/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/BasicSyntaxConditionalStatementsAndLoops_Exercise/05.Login/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = ReverseString(username);
+            int tries = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input==password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                tries++;
+                if (tries==4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");            
+            }         
+        }
+    }
+}
