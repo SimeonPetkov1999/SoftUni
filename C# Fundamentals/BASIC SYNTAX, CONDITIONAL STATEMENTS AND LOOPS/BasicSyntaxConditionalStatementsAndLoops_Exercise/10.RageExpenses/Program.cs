ceb72c3ac457b0b588dcb72ed9f561e774a581e0
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-18 11:55:56 +0300

    HW Basic Syntax

---
+++ C# Fundamentals/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/BasicSyntaxConditionalStatementsAndLoops_Exercise/10.RageExpenses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _10.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int trashedHeadsets = 0;
+            int trashedMouses = 0;
+            int trashedKeyboards = 0;
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    trashedHeadsets++;
+                }
+
+                if (i % 3 == 0)
+                {
+                    trashedMouses++;
+                }
+
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    trashedKeyboards++;
+                }               
+            }
+
+            double totalPrice = (trashedHeadsets * headsetPrice) + (trashedMouses * mousePrice) + (trashedKeyboards * keyboardPrice) + (displayPrice * (Math.Round(trashedKeyboards*1.0 / 2)));
+
+            Console.WriteLine($"Rage expenses: {totalPrice:F2} lv.");
+        }
+    }
+}
