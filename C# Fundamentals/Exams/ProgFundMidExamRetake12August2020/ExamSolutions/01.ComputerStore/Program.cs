446bc5ca150dcd1f61cd55c526149d52be06e6d2
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-18 20:46:56 +0300

    Practice exam

---
+++ C# Fundamentals/Exams/ProgFundMidExamRetake12August2020/ExamSolutions/01.ComputerStore/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01.ComputerStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            double priceNoTaxes = 0;
+            double taxes = 0;
+            double totalPrice = 0;
+
+            while (input != "regular" && input != "special")
+            {
+                double money = double.Parse(input);
+                if (money < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                priceNoTaxes += money;
+                taxes = taxes + (money * 0.20);             
+                input = Console.ReadLine();
+            }
+
+            totalPrice = priceNoTaxes + taxes;
+
+            if (input=="special")
+            {
+                totalPrice *= 0.90;
+            }
+
+            if (totalPrice==0)
+            {
+                Console.WriteLine("Invalid order!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {priceNoTaxes:f2}$");
+            Console.WriteLine($"Taxes: {taxes:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalPrice:f2}$");
+        }
+    }
+}
