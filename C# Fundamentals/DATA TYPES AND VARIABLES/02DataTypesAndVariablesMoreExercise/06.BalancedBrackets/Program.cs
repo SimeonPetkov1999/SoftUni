434a9f71518047260e32ce81e9444653f7a5c0f6
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-21 20:23:41 +0300

    HW

--- C# Fundamentals/DATA TYPES AND VARIABLES/02DataTypesAndVariablesMoreExercise/06.BalancedBrackets/Program.cs
+++ C# Fundamentals/DATA TYPES AND VARIABLES/02DataTypesAndVariablesMoreExercise/06.BalancedBrackets/Program.cs
@@ -11,17 +11,16 @@ namespace _06.BalancedBrackets
             int numberOfClosingBrackets = 0;
             int checkForTwoOpenings = 0;
             int checkForTwoClosings = 0;
-
             for (int i = 0; i < n; i++)
             {
                 string input = Console.ReadLine();
-                if (input=="(" || input ==")")
+                if (input == "(" || input == ")")
                 {
                     if (input == "(")
                     {
                         checkForTwoClosings = 0;
                         checkForTwoOpenings++;
-                        if (checkForTwoOpenings==2)
+                        if (checkForTwoOpenings == 2)
                         {
                             Console.WriteLine("UNBALANCED");
                             Environment.Exit(0);
@@ -30,7 +29,7 @@ namespace _06.BalancedBrackets
                     }
                     else if (input == ")")
                     {
-                        
+
                         checkForTwoOpenings = 0;
                         checkForTwoClosings++;
                         if (checkForTwoClosings == 2)
@@ -40,7 +39,7 @@ namespace _06.BalancedBrackets
                         }
                         numberOfClosingBrackets++;
                     }
-                }            
+                }
             }
             if (numberOfClosingBrackets == numberOfOpeningBrackets)
             {
@@ -49,7 +48,7 @@ namespace _06.BalancedBrackets
             else
             {
                 Console.WriteLine("UNBALANCED");
-            }       
+            }
         }
     }
 }
