02884156a84d3163100252fd1463eb5d46e8a911
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-26 16:05:19 +0300

    arrays exercise

---
+++ C# Fundamentals/ARRAYS/03ArraysLab/07.EqualArrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            int index = 0;
+            bool Equal = true;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] == secondArray[i])
+                {
+                    sum += firstArray[i];
+                }
+                else
+                {
+                    Equal = false;
+                    index = i;
+                    break;
+                }
+            }
+            if (Equal)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {index} index");
+            }
+
+        }
+    }
+}
