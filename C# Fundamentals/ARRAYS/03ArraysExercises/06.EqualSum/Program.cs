ee2f7dd35077acc932b00de52bd20ee3543bdb01
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-27 22:02:55 +0300

    arrays

---
+++ C# Fundamentals/ARRAYS/03ArraysExercises/06.EqualSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+
+                for (int k = i + 1; k < numbers.Length; k++)
+                {
+                    rightSum += numbers[k];
+                }
+
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(i);
+                    Environment.Exit(0);
+                }
+            }
+            Console.WriteLine("no");
+
+        }
+    }
+}
---
+++ C# Fundamentals/ARRAYS/03ArraysExercises/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int number=int.MinValue;
+            int count = 0;
+ 
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] == numbers[i+1])
+                {
+                    number = numbers[i];
+                    count++;
+                }
+                else
+                {
+
+                }
+            }
+
+            for (int i = 0; i <count ; i++)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
