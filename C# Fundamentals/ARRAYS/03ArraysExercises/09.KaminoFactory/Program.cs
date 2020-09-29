5b538ec0ac69290eb5eb3e7b763ff6b7408c2af3
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-29 18:47:38 +0300

    arrays exercises 9

--- C# Fundamentals/ARRAYS/03ArraysExercises/09.KaminoFactory/Program.cs
+++ C# Fundamentals/ARRAYS/03ArraysExercises/09.KaminoFactory/Program.cs
@@ -12,21 +12,80 @@ namespace _09.KaminoFactory
             string input = Console.ReadLine();
             int[] bestDna = new int[lenght];
             int bestIndex = int.MinValue;
-            int bestSequenceNum = 0;
+            int bestSum = 0;
+            int bestSequenceLenght = 0;
+            int currentSequenceLenght = 1;
+            int currentSum = 0;
+            int sample = 0;
+            int bestSample = 0;
+            int currentIndex = int.MinValue;
 
-            while (input!="Clone them!")
+            bool checkForFirstIndex = false;
+
+            while (input != "Clone them!")
             {
-                int[] numbers = input.Split('!').Select(int.Parse).ToArray();
+                int[] numbers = input.Split('!', StringSplitOptions.RemoveEmptyEntries).
+                    Select(int.Parse).
+                    ToArray();
+                sample++;
 
-                for (int i = 0; i < numbers.Length-1; i++)
+                for (int i = 0; i < numbers.Length - 1; i++)
                 {
-                    if (numbers[i] ==1 && numbers[i+1] == 1)
+                    if (numbers[i] == 1 && numbers[i + 1] ==1)
                     {
-                        
+                        currentSequenceLenght++;
+                        if (!checkForFirstIndex)
+                        {
+                            currentIndex = i;
+                            checkForFirstIndex = true;
+                        }
+                    }                         
+                }
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i]==1)
+                    {
+                        currentSum++;
                     }
                 }
-
+                if (currentSequenceLenght > bestSequenceLenght)
+                {
+                    bestDna = numbers;
+                    bestSum = currentSum;
+                    bestSample = sample;
+                    bestIndex = currentIndex;
+                    bestSequenceLenght = currentSequenceLenght;
+                }
+                if (currentSequenceLenght == bestSequenceLenght)
+                {
+                    if (currentIndex < bestIndex)
+                    {
+                        bestDna = numbers;
+                        bestSum = currentSum;
+                        bestSample = sample;
+                        bestIndex = currentIndex;
+                        bestSequenceLenght = currentSequenceLenght;
+                    }
+                }
+                if ((currentSequenceLenght == bestSequenceLenght) && (currentIndex == bestIndex))
+                {
+                    if (currentSum > bestSum)
+                    {
+                        bestDna = numbers;
+                        bestSum = currentSum;
+                        bestSample = sample;
+                        bestIndex = currentIndex;
+                        bestSequenceLenght = currentSequenceLenght;
+                    }
+                }
+                checkForFirstIndex = false;
+                currentIndex = 0;
+                currentSequenceLenght = 1;
+                currentSum = 0;
+                input = Console.ReadLine();
             }
+            Console.WriteLine($"Best DNA sample {bestSample} with sum: {bestSum}.");
+            Console.WriteLine(string.Join(" ", bestDna));
         }
     }
 }
---
+++ C# Fundamentals/ARRAYS/03ArraysExercises/10.LadyBugs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] field = new int[size];
+
+            int[] ladybugsIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < ladybugsIndexes.Length; i++)
+            {
+                if (i==ladybugsIndexes[i])
+                {
+                    field[i] = 1;
+                }
+            }
+
+            string input = Console.ReadLine();
+            while (input!="end")
+            {
+                string[] commands = input.Split();
+                string leftOrRight = commands[1];
+                int indexOfBug = int.Parse(commands[0].ToString());
+                int flightLenght = int.Parse(commands[2].ToString());
+
+                if (leftOrRight=="right")
+                {
+                    
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", field));
+
+        }
+    }
+}
