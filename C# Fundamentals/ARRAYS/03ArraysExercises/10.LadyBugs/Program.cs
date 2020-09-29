3bbeb9b2223998f14080dc27649f98bd133583f0
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-09-29 21:10:28 +0300

    arrays exercises 10

--- C# Fundamentals/ARRAYS/03ArraysExercises/10.LadyBugs/Program.cs
+++ C# Fundamentals/ARRAYS/03ArraysExercises/10.LadyBugs/Program.cs
@@ -14,30 +14,64 @@ namespace _10.LadyBugs
 
             for (int i = 0; i < ladybugsIndexes.Length; i++)
             {
-                if (i==ladybugsIndexes[i])
+                if (ladybugsIndexes[i] >= 0 && ladybugsIndexes[i] < size)
                 {
-                    field[i] = 1;
+                    field[ladybugsIndexes[i]] = 1;
                 }
             }
 
-            string input = Console.ReadLine();
-            while (input!="end")
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
             {
                 string[] commands = input.Split();
                 string leftOrRight = commands[1];
                 int indexOfBug = int.Parse(commands[0].ToString());
                 int flightLenght = int.Parse(commands[2].ToString());
 
-                if (leftOrRight=="right")
+                if (indexOfBug < 0 || indexOfBug > field.Length - 1 || field[indexOfBug] == 0)
                 {
-                    
+                    continue;
+                }
+                if (leftOrRight == "right")
+                {
+                    int targetIndex = indexOfBug + flightLenght;
+                    field[indexOfBug] = 0;
+                    while (targetIndex<field.Length)
+                    {
+                        if (field[targetIndex]==1)
+                        {
+                            targetIndex += targetIndex;
+                        }
+                        else
+                        {
+                            field[targetIndex] = 1;
+                            break;
+                        }
+                    }
                 }
 
-                input = Console.ReadLine();
+                else if (leftOrRight == "left")
+                {
+                    int targetIndex = indexOfBug - flightLenght;
+                    field[indexOfBug] = 0;
+
+                    while (targetIndex >= 0)
+                    {
+                        if (field[targetIndex] == 1)
+                        {
+                            targetIndex -= targetIndex;
+                        }
+
+                        else
+                        {
+                            field[targetIndex] = 1;
+                            break;
+                        }
+                    }
+                }               
             }
-
             Console.WriteLine(string.Join(" ", field));
-
-        }
+         }
     }
 }
