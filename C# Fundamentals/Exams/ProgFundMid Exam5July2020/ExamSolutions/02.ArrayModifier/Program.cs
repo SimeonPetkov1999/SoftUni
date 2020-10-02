e3eee18f8a6d6808ed6523cceff624ecce62a682
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-02 20:42:16 +0300

    arrays exam

---
+++ C# Fundamentals/Exams/ProgFundMid Exam5July2020/ExamSolutions/02.ArrayModifier/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] commands = input.Split();
+                string command;
+                int index1 = 0;
+                int index2 = 0;
+                if (input != "decrease")
+                {
+
+                    command = commands[0];
+                    index1 = int.Parse(commands[1]);
+                    index2 = int.Parse(commands[2]);
+                }
+                else
+                {
+                    command = input;
+                }
+                switch (command)
+                {
+                    case "swap":
+                        int temp = numbers[index1];
+                        numbers[index1] = numbers[index2];
+                        numbers[index2] = temp;
+                        break;
+                    case "multiply":
+                        numbers[index1] = numbers[index1] * numbers[index2];
+                        break;
+                    case "decrease":
+                        for (int i = 0; i < numbers.Length; i++)
+                        {
+                            numbers[i] = numbers[i] - 1;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}
