be7e1f2449bacb690fd324f03bfc98b657fba554
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-02 20:19:12 +0300

    arrays More exercises

--- C# Fundamentals/ARRAYS/03ArraysMoreExercises/04.FoldAndSum/Program.cs
+++ C# Fundamentals/ARRAYS/03ArraysMoreExercises/04.FoldAndSum/Program.cs
@@ -13,31 +13,30 @@ namespace _04.FoldAndSum
                 .ToArray();
 
             int k = numbers.Length / 4;
+            int[] firstRow = new int[2 * k];
+            int[] secondRow = new int[2 * k];
+            int[] sumArray = new int[2 * k];
 
-            int[] firstRow = new int[2*k];
-            int[] secondRow = new int[2*k];
-            int[] firstLeftRow = new int[k];
-            int[] firstRightRow = new int[k];
-
-            ///int temp = k - 1;
-            ///for (int i = 0; i < k; i++)
-            ///{
-            ///    firstRow[temp--] = numbers[i];
-            ///}
-            ///
-
+            int temp = k - 1;
             for (int i = 0; i < k; i++)
             {
-                firstLeftRow[i] = numbers[i];
+                firstRow[temp--] = numbers[i];
             }
-
-            for (int i = 0; i < k; i++)
+            int temp2 = firstRow.Length - k;
+            for (int i = numbers.Length - 1; i >= numbers.Length - k; i--)
             {
-                firstRightRow[i] = numbers[i];
+                firstRow[temp2++] = numbers[i];
             }
-
-
-
+            int index = 0;
+            for (int i = k; i < numbers.Length - k; i++)
+            {
+                secondRow[index++] = numbers[i];
+            }
+            for (int i = 0; i <sumArray.Length; i++)
+            {
+                sumArray[i] = firstRow[i] + secondRow[i];
+            }
+            Console.WriteLine(string.Join(' ',sumArray));
         }
     }
 }
