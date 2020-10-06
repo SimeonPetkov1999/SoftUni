10dce3723ad87b85f12875f7357d8425af9bfda3
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-06 19:44:04 +0300

    Methods lab

---
+++ C# Fundamentals/METHODS/MethodsLab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _10.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(GetMultipleOfEvenAndOdds(GetSumOfOddDigits(input),GetSumOfEvenDigits(input)));
+          }
+        static int GetSumOfOddDigits(string input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]=='-')
+                {
+                    continue;
+                }
+                int currentDigit = int.Parse(input[i].ToString());
+                if (currentDigit % 2 != 0)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int GetSumOfEvenDigits(string input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '-')
+                {
+                    continue;
+                }
+                int currentDigit = int.Parse(input[i].ToString());
+                if (currentDigit % 2 == 0)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int GetMultipleOfEvenAndOdds(int odd, int even)
+        {
+            return odd * even;
+        }
+    }
+}
---
+++ C# Fundamentals/METHODS/MethodsLab/11.Mathoperations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11.Mathoperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            char operattor = char.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculator(firstNum,secondNum,operattor));
+        }
+
+        static double Calculator(double num1, double num2, char operattor) 
+        {
+            double result=0;
+            switch (operattor)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            return result;
+        }
+    }
+}
