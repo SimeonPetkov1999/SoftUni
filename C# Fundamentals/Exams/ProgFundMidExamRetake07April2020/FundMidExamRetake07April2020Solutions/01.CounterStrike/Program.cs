c9bf3a330427db8158cf319269d1108cbf56e2fa
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-10 15:19:48 +0300

    Exams

---
+++ C# Fundamentals/Exams/ProgFundMidExamRetake07April2020/FundMidExamRetake07April2020Solutions/01.CounterStrike/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01.CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            int battlesWon = 0;
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(input);             
+                if (initialEnergy - distance >= 0)
+                {
+                    initialEnergy -= distance;
+                    battlesWon++;    
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesWon} won battles and {initialEnergy} energy");
+                    Environment.Exit(0);
+                }
+                if (battlesWon % 3 == 0)
+                {
+                    initialEnergy += battlesWon;
+                }
+            }
+            Console.WriteLine($"Won battles: {battlesWon}. Energy left: {initialEnergy}");
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/ProgFundMidExamRetake07April2020/FundMidExamRetake07April2020Solutions/02.ShootForTheWin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.ShootForTheWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input = string.Empty;
+            int shotTargets = 0;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                int indexOfTarget = int.Parse(input);
+                int pointsOfTarget;
+
+                if (indexOfTarget < 0 || indexOfTarget > targets.Length - 1)
+                {
+                    continue;
+                }
+
+                if (targets[indexOfTarget] != -1)
+                {
+                    pointsOfTarget = targets[indexOfTarget];
+                    targets[indexOfTarget] = -1;
+                    shotTargets++;
+                    for (int i = 0; i < targets.Length; i++)
+                    {
+                        if (targets[i] != -1 && targets[i] > pointsOfTarget)
+                        {
+                            targets[i] = targets[i] - pointsOfTarget;
+                        }
+
+                        else if (targets[i] != -1 && targets[i] <= pointsOfTarget)
+                        {
+                            targets[i] = targets[i] + pointsOfTarget;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Shot targets: {shotTargets} -> " + string.Join(" ", targets));
+        }
+    }
+}
