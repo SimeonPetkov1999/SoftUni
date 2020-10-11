24cdc881937920ce12185e5f9438da0ffb879135
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-11 16:04:26 +0300

    Lists Lab

---
+++ C# Fundamentals/LISTS/ListsLab/03.MergingLists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> resultList = new List<int>();
+            int maxCount = Math.Max(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                if (i<firstList.Count)
+                {
+                    resultList.Add(firstList[i]);
+                }
+                if (i<secondList.Count)
+                {
+                    resultList.Add(secondList[i]);
+                }              
+            }
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
