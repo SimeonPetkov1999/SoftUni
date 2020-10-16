afcaa6d0da48e563c54b2cd5ed99bd15d9023905
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-16 20:01:40 +0300

    exam

---
+++ C# Fundamentals/Exams/ProgFundMidExamRetake07April2020/FundMidExamRetake07April2020Solutions/03.MovingTarget/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MovingTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<string> command = Console.ReadLine().Split().ToList();
+
+            while(command[0] != "End")
+            {
+                if (command[0]=="Shoot")
+                {
+                    int index = int.Parse(command[1]);
+                    int power = int.Parse(command[2]);
+
+                    if (index>=0 && index<=targets.Count)
+                    {
+                        targets[index] -= power;
+                        if (targets[index]<=0)
+                        {
+                            targets.RemoveAt(index);
+                        }
+                    }
+                }
+
+                else if (command[0] =="Add")
+                {
+                    int index = int.Parse(command[1]);
+                    int value = int.Parse(command[2]);
+
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets.Insert(index, value); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                }
+                else if (command[0]=="Strike")
+                {
+                    int index = int.Parse(command[1]);
+                    int radius = int.Parse(command[2]);
+
+                    if (index - radius < 0 || index + radius >=targets.Count)
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+
+                    else
+                    {
+                        targets.RemoveRange(index - radius, radius * 2 + 1);
+                    }
+                }
+                command = Console.ReadLine().Split().ToList();
+            }
+
+            Console.WriteLine(string.Join('|',targets));
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/ProgFundMidExamRetake12August2020/ExamSolutions/03.MemoryGame/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> elements = Console.ReadLine().Split().ToList();
+
+            List<string> input = Console.ReadLine().Split().ToList();
+            int moves = 0;
+            while (input[0] != "end")
+            {
+                int index1 = int.Parse(input[0]);
+                int index2 = int.Parse(input[1]);
+
+                if (index1 == index2)
+                {
+                    moves++;
+                    AddingElementsToTheList(elements, moves);
+                    input = Console.ReadLine().Split().ToList();
+                    continue;
+
+                }
+                else if (index1 < 0 || index1 >= elements.Count)
+                {
+                    moves++;
+                    AddingElementsToTheList(elements, moves);
+                    input = Console.ReadLine().Split().ToList();
+                    continue;
+                }
+                else if (index2 < 0 || index2 >= elements.Count)
+                {
+                    moves++;
+                    AddingElementsToTheList(elements, moves);
+                    input = Console.ReadLine().Split().ToList();
+                    continue;
+                }
+
+                if (elements[index1] == elements[index2])
+                {
+                    Console.WriteLine($"Congrats! You have found matching elements - {elements[index1]}!");
+                    moves++;
+                    elements.RemoveAt(Math.Max(index1, index2));
+                    elements.RemoveAt(Math.Min(index1, index2));
+
+                }
+
+                else if (elements[index1] != elements[index2])
+                {
+                    Console.WriteLine("Try again!");
+                    moves++;
+                }
+
+                if (elements.Count == 0)
+                {
+                    Console.WriteLine($"You have won in {moves} turns!");
+                     Environment.Exit(0);
+                }
+                input = Console.ReadLine().Split().ToList();
+            }
+
+            Console.WriteLine("Sorry you lose :(");
+            Console.WriteLine(string.Join(" ",elements));
+           
+        }
+        static void AddingElementsToTheList(List<string> elements, int moves)
+        {
+            Console.WriteLine("Invalid input! Adding additional elements to the board");
+            int middle = elements.Count / 2;
+            elements.Insert(middle, $"-{moves}a");
+            elements.Insert(middle, $"-{moves}a");
+        }
+    }
+}
