272adc42730bae83f8a69a311f10f3853deafabd
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-10-31 15:16:43 +0200

    added exam

--- C# Fundamentals/Exams/TechFundMidExam-16April2019/ExamSolution/02.EasterGifts/Program.cs
+++ C# Fundamentals/Exams/TechFundMidExam-16April2019/ExamSolution/02.EasterGifts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.EasterGifts
 {
@@ -6,7 +7,49 @@ namespace _02.EasterGifts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] giftsList = Console.ReadLine().Split();
+
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0] != "No")
+            {
+                string gift = command[1];
+
+                switch (command[0])
+                {
+                    case "OutOfStock":
+                        if (giftsList.Contains(gift))
+                        {
+                            for (int i = 0; i < giftsList.Length; i++)
+                            {
+                                if (giftsList[i] == gift)
+                                {
+                                    giftsList[i] = "None";
+                                }
+                            }                       
+                        }
+                        break;
+                    case "Required":
+                        int index = int.Parse(command[2]);
+                        if (index >= 0 && index <= giftsList.Length - 1)
+                        {
+                            giftsList[index] = gift;
+                        }
+                        break;
+                    case "JustInCase":
+                        giftsList[giftsList.Length - 1] = gift;
+                        break;
+                }
+                command = Console.ReadLine().Split();
+            }
+
+            for (int i = 0; i < giftsList.Length; i++)
+            {
+                if (giftsList[i] != "None")
+                {
+                    Console.Write($"{giftsList[i]} ");
+                }
+            }
         }
     }
 }
---
+++ C# Fundamentals/Exams/TechFundMidExam-16April2019/ExamSolution/03.EasterShopping/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.EasterShopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shops = Console.ReadLine().Split().ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commands = Console.ReadLine().Split();
+
+                switch (commands[0])
+                {
+                    case "Include":
+                        string shop = commands[1];
+                        shops.Add(shop);
+                        break;
+                    case "Visit":
+                        string firstLast = commands[1];
+                        int numberOfShops = int.Parse(commands[2]);
+
+                        if (shops.Count >= numberOfShops)
+                        {
+                            if (firstLast == "first")
+                            {
+                                shops.RemoveRange(0, numberOfShops);
+                            }
+
+                            else if (firstLast == "last")
+                            {
+                                shops.RemoveRange(shops.Count - numberOfShops, numberOfShops);
+                            }
+                        }
+                        break;
+                    case "Prefer":
+                        int index1 = int.Parse(commands[1]);
+                        int index2 = int.Parse(commands[2]);
+
+                        if ((index1 >= 0 && index1 <= shops.Count - 1) && ((index2 >= 0 && index2 <= shops.Count - 1)))
+                        {
+                            string temp = shops[index1];
+                            shops[index1] = shops[index2];
+                            shops[index2] = temp;
+                        }
+                        break;
+                    case "Place":
+                        int index = int.Parse(commands[2]);
+                        index++;
+                        if (index >= 0 && index <= shops.Count - 1)
+                        {
+                            shops.Insert(index, commands[1]);
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine("Shops left:");
+            Console.WriteLine(string.Join(" ",shops));
+        }
+    }
+}
