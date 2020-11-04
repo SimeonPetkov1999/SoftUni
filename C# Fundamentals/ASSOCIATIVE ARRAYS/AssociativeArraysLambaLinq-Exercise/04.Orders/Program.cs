d4459e1612359790d14361d2cc3085006db1ca4e
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-04 21:26:22 +0200

    added Dictionaries exercise

---
+++ C# Fundamentals/ASSOCIATIVE ARRAYS/AssociativeArraysLambaLinq-Exercise/04.Orders/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, CMyPrice> orders = new Dictionary<string, CMyPrice>();
+
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0]!="buy")
+            {
+                string item = command[0];
+                double price = double.Parse(command[1]);
+                double quantity = double.Parse(command[2]);
+
+                if (!orders.ContainsKey(item))
+                {
+                    orders.Add(item, new CMyPrice());
+                    orders[item].Price = price;
+                    orders[item].Quantity = quantity;
+                }
+
+                else
+                {
+                    orders[item].Price = price;
+                    orders[item].Quantity += quantity;
+                }
+                command = Console.ReadLine().Split();
+            }
+
+            foreach (var item in orders)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.calculate():f2}");
+            }
+        }
+    }
+    public class CMyPrice 
+    {
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+        public double calculate()
+        {
+          return Price*Quantity;
+        }
+    }
+}
---
+++ C# Fundamentals/ASSOCIATIVE ARRAYS/AssociativeArraysLambaLinq-Exercise/05.SoftUniParking/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cars = new Dictionary<string, string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0]=="register")
+                {
+                    string username = command[1];
+                    string plate = command[2];
+
+                    if (!cars.ContainsKey(username))
+                    {
+                        cars.Add(username, plate);
+                        Console.WriteLine($"{username} registered {plate} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {plate}");
+                    }
+                }
+
+                else if (command[0]=="unregister")
+                {
+                    string username = command[1];
+
+                    if (!cars.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        cars.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
