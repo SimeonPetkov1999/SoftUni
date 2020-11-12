358ea26abd487947c2ce345ab2ec9bba74374758
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-12 21:53:52 +0200

    updated with metods and class

--- C# Fundamentals/Exams/ProgFundFinalExam04April2020G2/ExamSolutions/03.HeroesOfCodeAndLogicVII/Program.cs
+++ C# Fundamentals/Exams/ProgFundFinalExam04April2020G2/ExamSolutions/03.HeroesOfCodeAndLogicVII/Program.cs
@@ -8,7 +8,7 @@ namespace _03.HeroesOfCodeAndLogicVII
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<int>> heroes = new Dictionary<string, List<int>>();
+            Dictionary<string, Stats> heroes = new Dictionary<string, Stats>();
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
@@ -20,9 +20,7 @@ namespace _03.HeroesOfCodeAndLogicVII
 
                 if (!heroes.ContainsKey(heroName))
                 {
-                    heroes[heroName] = new List<int>(2);
-                    heroes[heroName].Add(HP);
-                    heroes[heroName].Add(MP);
+                    heroes[heroName] = new Stats(HP,MP);
                 }
             }
 
@@ -32,80 +30,110 @@ namespace _03.HeroesOfCodeAndLogicVII
                 string action = command[0];
                 if (action == "CastSpell")
                 {
-                    string heroName = command[1];
-                    int MPneeded = int.Parse(command[2]);
-                    string spellName = command[3];
-
-                    if (heroes[heroName][1] >= MPneeded)
-                    {
-                        heroes[heroName][1] -= MPneeded;
-                        Console.WriteLine($"{heroName} has successfully cast {spellName} and now has {heroes[heroName][1]} MP!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{heroName} does not have enough MP to cast {spellName}!");
-                    }
+                    CastSpell(command, heroes);
                 }
 
                 else if (action == "TakeDamage")
                 {
-                    string heroName = command[1];
-                    int damage = int.Parse(command[2]);
-                    string attacker = command[3];
-
-                    if (heroes[heroName][0] - damage > 0)
-                    {
-                        heroes[heroName][0] -= damage;
-                        Console.WriteLine($"{heroName} was hit for {damage} HP by {attacker} and now has {heroes[heroName][0]} HP left!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{heroName} has been killed by {attacker}!");
-                        heroes.Remove(heroName);
-                    }
+                    TakeDamage(command, heroes);
                 }
                 else if (action == "Recharge")
                 {
-                    string heroName = command[1];
-                    int amount = int.Parse(command[2]);
-
-                    if (heroes[heroName][1] + amount <= 200)
-                    {
-                        heroes[heroName][1] += amount;
-                        Console.WriteLine($"{heroName} recharged for {amount} MP!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{heroName} recharged for {200 - heroes[heroName][1]} MP!");
-                        heroes[heroName][1] = 200;
-                    }
+                    Recharge(command, heroes);
                 }
 
                 else if (action == "Heal")
                 {
-                    string heroName = command[1];
-                    int amount = int.Parse(command[2]);
-
-                    if (heroes[heroName][0] + amount <= 100)
-                    {
-                        heroes[heroName][0] += amount;
-                        Console.WriteLine($"{heroName} healed for {amount} HP!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{heroName} healed for {100 - heroes[heroName][0]} HP!");
-                        heroes[heroName][0] = 100;
-                    }
+                    Heal(command,heroes);
                 }
                 command = Console.ReadLine().Split(" - ");
             }
 
-            foreach (var kvp in heroes.OrderByDescending(n => n.Value[0]).ThenBy(n => n.Key))
+            foreach (var kvp in heroes.OrderByDescending(n => n.Value.HP).ThenBy(n => n.Key))
             {
                 Console.WriteLine($"{kvp.Key}");
-                Console.WriteLine($"  HP: {kvp.Value[0]}");
-                Console.WriteLine($"  MP: {kvp.Value[1]}");
+                Console.WriteLine($"  HP: {kvp.Value.HP}");
+                Console.WriteLine($"  MP: {kvp.Value.MP}");
+            }
+        }
 
+        class Stats
+        {
+            public Stats(int HP, int MP)
+            {
+                this.HP = HP;
+                this.MP = MP;
+            }
+            public int HP { get; set; }
+            public int MP { get; set; }
+        }
+
+        static void CastSpell(string[] command, Dictionary<string, Stats> heroes) 
+        {
+            string heroName = command[1];
+            int MPneeded = int.Parse(command[2]);
+            string spellName = command[3];
+
+            if (heroes[heroName].MP >= MPneeded)
+            {
+                heroes[heroName].MP -= MPneeded;
+                Console.WriteLine($"{heroName} has successfully cast {spellName} and now has {heroes[heroName].MP} MP!");
+            }
+            else
+            {
+                Console.WriteLine($"{heroName} does not have enough MP to cast {spellName}!");
+            }
+        }
+
+        static void TakeDamage(string[] command, Dictionary<string, Stats> heroes) 
+        {
+            string heroName = command[1];
+            int damage = int.Parse(command[2]);
+            string attacker = command[3];
+
+            if (heroes[heroName].HP - damage > 0)
+            {
+                heroes[heroName].HP -= damage;
+                Console.WriteLine($"{heroName} was hit for {damage} HP by {attacker} and now has {heroes[heroName].HP} HP left!");
+            }
+            else
+            {
+                Console.WriteLine($"{heroName} has been killed by {attacker}!");
+                heroes.Remove(heroName);
+            }
+        }
+
+        static void Recharge(string[] command, Dictionary<string, Stats> heroes) 
+        {
+            string heroName = command[1];
+            int amount = int.Parse(command[2]);
+
+            if (heroes[heroName].MP + amount <= 200)
+            {
+                heroes[heroName].MP += amount;
+                Console.WriteLine($"{heroName} recharged for {amount} MP!");
+            }
+            else
+            {
+                Console.WriteLine($"{heroName} recharged for {200 - heroes[heroName].MP} MP!");
+                heroes[heroName].MP = 200;
+            }
+        }
+
+        static void Heal(string[] command, Dictionary<string, Stats> heroes) 
+        {
+            string heroName = command[1];
+            int amount = int.Parse(command[2]);
+
+            if (heroes[heroName].HP + amount <= 100)
+            {
+                heroes[heroName].HP += amount;
+                Console.WriteLine($"{heroName} healed for {amount} HP!");
+            }
+            else
+            {
+                Console.WriteLine($"{heroName} healed for {100 - heroes[heroName].HP} HP!");
+                heroes[heroName].HP = 100;
             }
         }
     }
