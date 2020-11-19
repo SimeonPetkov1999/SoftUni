40f1dce699d6973428344f7c4a813792fd53a9a1
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-19 22:09:07 +0200

    added regex exercise

---
+++ C# Fundamentals/REGULAR EXPRESSIONS/RegexExercise/01.Furniture/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @">>(?<name>[A-Za-z\s]+)<<(?<price>[0-9]*(.\d+)?)[0-9]*!(?<quantity>\d+)";
+
+            string input = Console.ReadLine();
+
+            Dictionary<string, double> allMatches = new Dictionary<string, double>();
+
+            while (input != "Purchase")
+            {
+                Match match = Regex.Match(input, regex);
+                if (match.Success)
+                {
+                    var type = match.Groups["name"].ToString();
+                    double money = double.Parse(match.Groups["price"].Value) * int.Parse(match.Groups["quantity"].Value);
+                    allMatches.Add(type, money);
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Bought furniture: ");
+            foreach (var item in allMatches)
+            {
+                Console.WriteLine(item.Key);
+            }
+            double totalSum = allMatches.Values.Sum();
+            Console.WriteLine($"Total money spend: {totalSum:f2}");
+        }
+    }
+}
