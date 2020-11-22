643c525cc16e4e805c4115472bf34d93325f0b81
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-22 21:26:22 +0200

    added exercise exam

---
+++ C# Fundamentals/Exams/ProgFundFinalExam04April2020G2/ExamSolutions/02.FancyBarcodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                var match = Regex.Match(input, @"@#+([A-Z]+([a-zA-Z0-9]+){4,}[A-Z])@#+");
+                if (match.Success)
+                {
+                    string group = string.Empty;
+                    var numbers = Regex.Matches(input, @"\d");
+                    if (numbers.Count==0)
+                    {
+                        group = "00";
+                    }
+                    else
+                    {
+                        foreach (Match item in numbers)
+                        {
+                            group += item.Value;
+                        }
+                    }
+                    Console.WriteLine($"Product group: {group}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+        }
+    }
+}
