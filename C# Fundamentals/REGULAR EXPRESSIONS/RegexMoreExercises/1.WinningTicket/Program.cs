226980580a5455e8824197d6654abab882e55858
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-26 17:58:03 +0200

    RegEx More exercises

---
+++ C# Fundamentals/REGULAR EXPRESSIONS/RegexMoreExercises/1.WinningTicket/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLine = Console.ReadLine()
+                .Split(new string[] { ",", " ", "\t" }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                if (inputLine[i].Length == 20)
+                {
+                    string leftSide = inputLine[i].Substring(0, 10);
+                    string rightSide = inputLine[i].Substring(10, 10);
+
+                    var leftMatch = Regex.Match(leftSide, @"\@{6,}|\${6,}|\^{6,}|\#{6,}");
+                    var rightMatch = Regex.Match(rightSide, @"\@{6,}|\${6,}|\^{6,}|\#{6,}");
+
+                    if (leftMatch.Length == 10 && rightMatch.Length == 10)
+                    {
+                        Console.WriteLine($"ticket \"{inputLine[i]}\" - 10{leftMatch.Groups[0].Value[0]} Jackpot!");
+                    }
+                    else if (leftMatch.Length >= 6 && rightMatch.Length >= 6)
+                    {
+                        var min = Math.Min(leftMatch.Length, rightMatch.Length);
+                        Console.WriteLine($"ticket \"{inputLine[i]}\" - {min}{leftMatch.Groups[0].Value[0]}");
+                     }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{inputLine[i]}\" - no match");
+                     }
+                }
+                else
+                {
+                    Console.WriteLine($"invalid ticket");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/REGULAR EXPRESSIONS/RegexMoreExercises/2.RageQuit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2.RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            var symbolsMatch = Regex.Matches(inputLine, @"[^0-9]+");
+            var digitsmatch = Regex.Matches(inputLine, @"\d+");
+            string allSymbols = string.Join("", symbolsMatch).ToLower();
+            Dictionary<char, int> uniques = new Dictionary<char, int>();            
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < digitsmatch.Count; i++)
+            {
+                int timesTorepeat = int.Parse(digitsmatch[i].Value);
+                for (int j = 0; j < timesTorepeat; j++)
+                {
+                    sb.Append(symbolsMatch[i]);
+                }
+            }
+            string output = sb.ToString().ToUpper();
+            foreach (var item in output)
+            {
+                if (!uniques.ContainsKey(item))
+                {
+                    uniques.Add(item, 1);
+                }
+            }
+            Console.WriteLine($"Unique symbols used: {uniques.Count}");
+            Console.WriteLine(output);
+        }
+    }
+}
