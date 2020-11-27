1165ee75f50f24287fbd105b178502de3c91d300
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-27 17:23:42 +0200

    added exercise exam

---
+++ C# Fundamentals/Exams/ProgFundFinalExamRetake10April2020/Exam Solutions/ExamSolutions/02.MirrorWords/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02.MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            var pairs = Regex.Matches(line, @"(@|#)(?<left>[A-Za-z]{3,})\1{2}(?<right>[A-Za-z]{3,})\1{1}");
+            List<string> mirors = new List<string>();
+
+            if (pairs.Count > 0)
+            {
+                Console.WriteLine($"{pairs.Count} word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"No word pairs found!");
+            }
+
+            foreach (Match item in pairs)
+            {
+                string left = item.Groups["left"].Value;
+                string right = item.Groups["right"].Value;
+                string reversed = ReverseString(right);
+                if (reversed == left)
+                {
+                    mirors.Add(left);
+                    mirors.Add(right);
+                }
+
+            }
+            if (mirors.Count > 0)
+            {
+                List<string> result = new List<string>();
+                for (int i = 0; i < mirors.Count; i += 2)
+                {
+                    result.Add($"{mirors[i]} <=> {mirors[i + 1]}");
+                }
+                Console.WriteLine($"The mirror words are: ");
+                Console.WriteLine(string.Join(", ", result));
+            }
+            else
+            {
+                Console.WriteLine("No mirror words!");
+            }
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
--- C# Fundamentals/Exams/ProgFundFinalExamRetake10April2020/Exam Solutions/ExamSolutions/03.NeedForSpeedIII/Program.cs
+++ C# Fundamentals/Exams/ProgFundFinalExamRetake10April2020/Exam Solutions/ExamSolutions/03.NeedForSpeedIII/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.NeedForSpeedIII
 {
@@ -6,7 +8,94 @@ namespace _03.NeedForSpeedIII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split("|");
+                string car = line[0];
+                int mileage = int.Parse(line[1]);
+                int fuel = int.Parse(line[2]);
+                cars.Add(car, new Car(mileage, fuel));//?
+            }
+
+            string[] commandsArgs = Console.ReadLine().Split(" : ");
+            while (commandsArgs[0]!="Stop")
+            {
+
+                string command = commandsArgs[0];
+                string car = commandsArgs[1];
+                if (command=="Drive")
+                {                 
+                    int distance = int.Parse(commandsArgs[2]);
+                    int fuel = int.Parse(commandsArgs[3]);
+
+                    if (cars[car].Fuel<fuel)
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+                    else
+                    {
+                        cars[car].Mileage += distance;
+                        cars[car].Fuel -= fuel;
+                        Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+                        if (cars[car].Mileage>100000)
+                        {
+                            Console.WriteLine($"Time to sell the {car}! ");
+                            cars.Remove(car);
+                        }
+                    }
+                }
+
+                else if (command=="Refuel")
+                {
+                    int fuel = int.Parse(commandsArgs[2]);
+                    if (cars[car].Fuel + fuel <= 75)
+                    {
+                        cars[car].Fuel += fuel;
+                        Console.WriteLine($"{car} refueled with {fuel} liters");
+                    }
+                    else
+                    {
+                        cars[car].Fuel = 75;
+                        Console.WriteLine($"{car} refueled with {75-fuel} liters");
+                    }
+                }
+                else if (command=="Revert")
+                {
+                    int kilometers = int.Parse(commandsArgs[2]);
+
+                    cars[car].Mileage -= kilometers;
+                    if (cars[car].Mileage<10000)
+                    {
+                        cars[car].Mileage = 10000;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{car} mileage decreased by {kilometers} kilometers");
+                    }
+                }
+                commandsArgs = Console.ReadLine().Split(" : ");
+            }
+
+            foreach (var item in cars.OrderByDescending(n=>n.Value.Mileage).ThenBy(n=>n.Key))
+            {
+                Console.WriteLine($"{item.Key} -> Mileage: {item.Value.Mileage} kms, Fuel in the tank: {item.Value.Fuel} lt.");
+            }
+
+        }
+        public class Car 
+        {
+
+            public Car(int Mileage, int Fuel) 
+            {
+                this.Mileage = Mileage;
+                this.Fuel = Fuel;
+            }
+            public int Mileage { get; set; }
+            public int Fuel { get; set; }
+
         }
     }
 }
