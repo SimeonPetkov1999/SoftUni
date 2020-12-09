e37d97885e41b2875277e63c094a154e5fd171cd
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-12-09 16:48:21 +0200

    Added Exam 15-08-2020

---
+++ C# Fundamentals/Exams/ProgFundFinalExamRetake15August2020/ExamSolutions/01.TheImitationGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.TheImitationGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] commandArgs = Console.ReadLine().Split("|");
+
+            while (commandArgs[0]!="Decode")
+            {
+                string command = commandArgs[0];
+                if (command=="Move")
+                {
+                    int numOfLetters = int.Parse(commandArgs[1]);
+                    string removed = input.Substring(0, numOfLetters);
+                    input = input.Insert(input.Length,removed);
+                    input = input.Remove(0, numOfLetters);
+                }
+                else if (command=="Insert")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    string value = commandArgs[2];
+                    input = input.Insert(index, value);
+                }
+                else if (command=="ChangeAll")
+                {
+                    string toChange = commandArgs[1];
+                    string replacement = commandArgs[2];
+                    input = input.Replace(toChange, replacement);
+                }
+                commandArgs = Console.ReadLine().Split("|");
+            }
+
+            Console.WriteLine($"The decrypted message is: {input}");
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/ProgFundFinalExamRetake15August2020/ExamSolutions/03.ThePianist/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ThePianist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Composer> composers = new Dictionary<string, Composer>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split("|");
+                string piece = input[0];
+                string composer = input[1];
+                string key = input[2];
+                composers.Add(piece, new Composer(composer, key));//?
+            }
+
+            string[] commandArgs = Console.ReadLine().Split("|");
+
+            while (commandArgs[0]!="Stop")
+            {
+                string command = commandArgs[0];
+                string piece = commandArgs[1];
+                if (command=="Add")
+                {
+                    string composer = commandArgs[2];
+                    string key = commandArgs[3];
+                    if (!composers.ContainsKey(piece))
+                    {
+                        composers.Add(piece, new Composer(composer, key));
+                        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{piece} is already in the collection!");
+                    }
+                }
+                else if (command=="Remove")
+                {
+                    if (composers.ContainsKey(piece))
+                    {
+                        composers.Remove(piece);
+                        Console.WriteLine($"Successfully removed {piece}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+                else if(command=="ChangeKey")
+                {
+                    string newKey = commandArgs[2];
+                    if (composers.ContainsKey(piece))
+                    {
+                        composers[piece].key = newKey;
+                        Console.WriteLine($"Changed the key of {piece} to {newKey}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+                commandArgs = Console.ReadLine().Split("|");
+            }
+
+            foreach (var item in composers.OrderBy(n=>n.Key).ThenBy(n=>n.Value.composer))
+            {
+                Console.WriteLine($"{item.Key} -> Composer: {item.Value.composer}, Key: {item.Value.key}");
+            }
+        }
+
+        public class Composer
+        {
+            public Composer(string composer, string key)
+            {
+                this.composer = composer;
+                this.key = key;
+            }
+            public string composer { get; set; }
+            public string key { get; set; }
+        }
+    }
+}
