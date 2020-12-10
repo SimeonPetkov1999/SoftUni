f2563f1f56bad23fba3bb3564aa110e9540e4c79
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-12-10 14:24:33 +0200

    Old Exams

---
+++ C# Fundamentals/Exams/OlderExams/Task 2/Solutions/Deciphering/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var match = Regex.Match(input, @"^[d-z{},|#]+");
+            string[] toReplace = Console.ReadLine().Split();
+            if (match.Success)
+            {           
+                string decoded = string.Join("", input.Select(n => (char)(n - 3))
+                    .ToArray())
+                    .Replace(toReplace[0],toReplace[1]);
+                Console.WriteLine(decoded);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/OlderExams/Task 2/Solutions/MessageTranslator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MessageTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                var match = Regex.Match(input, @"!([A-Z][a-z]{2,})!:\[([A-Za-z]{8,})\]");
+                if (match.Success)
+                {
+                    string command = match.Groups[1].Value;
+                    string encryptedMessage = string.Join(" ", match
+                        .Groups[2]
+                        .Value
+                        .Select(n => (int)n)
+                        .ToArray());
+                    Console.WriteLine($"{command}: {encryptedMessage}");
+                }
+                else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+            }
+        }
+    }
+}
