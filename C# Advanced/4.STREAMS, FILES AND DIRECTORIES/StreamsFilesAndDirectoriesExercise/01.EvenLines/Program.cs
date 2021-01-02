c6c83dd510696944292882f3842565c93d81e351
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-01-02 13:28:08 +0200

    Added Streams Files and Directories - Exercise

---
+++ C# Advanced/4.STREAMS, FILES AND DIRECTORIES/StreamsFilesAndDirectoriesExercise/01.EvenLines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("./text.txt");
+            var sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    var currentLine = input[i];
+                    for (int j = 0; j < currentLine.Length; j++)
+                    {
+                        if (char.IsPunctuation(currentLine[j]))
+                        {
+                            sb.Append('@');
+                        }
+                        else
+                        {
+                            sb.Append(currentLine[j]);
+                        }    
+                    }                 
+                    Console.WriteLine(string.Join(' ',sb.ToString().Split(' ').Reverse()));
+                    sb.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/4.STREAMS, FILES AND DIRECTORIES/StreamsFilesAndDirectoriesExercise/02.LineNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllLines("./text.txt");
+            for (int i = 0; i < file.Length; i++)
+            {
+                var currentLine = file[i];
+                int countLetters = GetCountOfLetters(currentLine);
+                int countMarks = GetCountOfMarks(currentLine);
+                string result = $"Line {i + 1}: - {currentLine} ({countLetters})({countMarks})";
+                File.AppendAllText("./output.txt",result);
+            }
+        }
+        static int GetCountOfLetters(string text) 
+        {
+            int count = 0;
+            foreach (var item in text)
+            {
+                if (char.IsLetter(item))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static int GetCountOfMarks(string text)
+        {
+            int count = 0;
+            foreach (var item in text)
+            {
+                if (char.IsPunctuation(item))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
