926616f1469cb61733ff5dd901cee27b6240fc51
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-01-14 20:16:22 +0200

    Added Basic Algorithms

---
+++ C# Advanced/10.BASIC ALGORITHMS/BasicAlgorithms/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(Sum(input,0));
+        }
+
+        public static int Sum(int[] arr, int index = 0) 
+        {
+            if (index==arr.Length)
+            {
+                return 0;
+            }
+            return arr[index] + Sum(arr, ++index);
+        }
+    }
+}
