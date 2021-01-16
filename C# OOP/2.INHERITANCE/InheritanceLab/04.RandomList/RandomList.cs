6771fc5001dd4cc13112a57c6383f0df7081ce8b
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-01-16 16:05:42 +0200

    Added Inheritance - Lab

---
+++ C# OOP/2.INHERITANCE/InheritanceLab/04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+        public string RandomString() 
+        {
+            var random = new Random();
+            var index = random.Next(0, this.Count);
+            var rndString = this[index];
+            this.RemoveAt(index);
+            return rndString;
+        } 
+    }
+}
---
+++ C# OOP/2.INHERITANCE/InheritanceLab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public Stack<string> Stack { get; set; }
+
+        public StackOfStrings()
+        {
+            this.Stack = new Stack<string>();
+        }
+
+        public bool IsEmpty() 
+        {
+            return this.Count == 0;
+        }
+        public Stack<string> AddRange<T>(ICollection<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Stack.Push(item);
+            }
+            return this.Stack;
+
+        }
+
+    }
+}
