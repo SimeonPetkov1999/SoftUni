974447c03f7ab60e1a5e6d60774f85d7d6b959dc
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-02-20 17:47:41 +0200

    Added C# Advanced Exam

---
+++ C# Advanced/EXAMS/C#AdvancedFinalExam20Feb2021/ExamSolutions/02.Warships/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Warships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            var matrix = new string[size, size];
+            var attacksCoordinates = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var allShipsAtFirst = 0;
+            var firstPlyerShips = 0;
+
+            var secondPlyerShips = 0;
+            var shipsDestroyed = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                var currentLine = Console.ReadLine().Split();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currentLine[col];
+                    if (matrix[row, col] == "<")
+                    {
+                        firstPlyerShips++;
+                        allShipsAtFirst++;
+                    }
+                    else if (matrix[row, col] == ">")
+                    {
+                        secondPlyerShips++;
+                        allShipsAtFirst++;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < attacksCoordinates.Length; i++)
+            {
+                var currentCoordinates = attacksCoordinates[i]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var row = int.Parse(currentCoordinates[0]);
+                var col = int.Parse(currentCoordinates[1]);
+
+                if (IsCoordinatesValid(row, col, size))
+                {
+                    if (matrix[row, col] == "#")
+                    {
+                        MineExplode(row, col, matrix);                      
+                    }
+                    else if (matrix[row, col] == ">" || matrix[row, col] == "<")
+                    {
+                        matrix[row, col] = "X";                      
+                    }
+                    shipsDestroyed = GetAllShipsLeft(matrix);
+                }
+
+                firstPlyerShips = GetPlyerShips(matrix,"<");
+                secondPlyerShips = GetPlyerShips(matrix, ">");
+
+                if (firstPlyerShips == 0)
+                {
+                    Console.WriteLine($"Player Two has won the game! {allShipsAtFirst-shipsDestroyed} ships have been sunk in the battle.");
+                    Environment.Exit(0);
+                }
+                else if (secondPlyerShips == 0)
+                {
+                    Console.WriteLine($"Player One has won the game! {allShipsAtFirst - shipsDestroyed} ships have been sunk in the battle.");
+                    Environment.Exit(0);
+                }
+            }
+
+            Console.WriteLine($"It's a draw! Player One has {firstPlyerShips} ships left. Player Two has {secondPlyerShips} ships left.");
+
+        }
+
+        private static int GetAllShipsLeft(string[,] matrix)
+        {
+            var ships = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            { 
+                for (int col = 0; col < matrix.GetLength(0); col++)
+                {
+                    if (matrix[row, col] == "<" || matrix[row, col] == ">")
+                    {
+                        ships++;
+                    }
+                }
+            }
+            return ships;
+        }
+
+        private static int GetPlyerShips(string[,] matrix,string player)
+        {
+            var ships = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            { 
+                for (int col = 0; col < matrix.GetLength(0); col++)
+                {
+                    if (matrix[row, col] == player)
+                    {
+                        ships++;
+                    }
+                }
+            }
+            return ships;
+        }
+
+        private static void MineExplode(int row, int col, string[,] matrix)
+        {
+            matrix[row, col] = "X";
+            if (IsCoordinatesValid(row - 1, col - 1, matrix.GetLength(0)))
+            {
+                matrix[row - 1, col - 1] = "X";
+            }
+            if (IsCoordinatesValid(row - 1, col, matrix.GetLength(0)))
+            {
+                matrix[row - 1, col] = "X";
+            }
+            if (IsCoordinatesValid(row - 1, col + 1, matrix.GetLength(0)))
+            {
+                matrix[row - 1, col + 1] = "X";
+            }
+            if (IsCoordinatesValid(row, col - 1, matrix.GetLength(0)))
+            {
+                matrix[row, col - 1] = "X";
+            }
+            if (IsCoordinatesValid(row, col + 1, matrix.GetLength(0)))
+            {
+                matrix[row, col + 1] = "X";
+            }
+            if (IsCoordinatesValid(row + 1, col - 1, matrix.GetLength(0)))
+            {
+                matrix[row + 1, col - 1] = "X";
+            }
+            if (IsCoordinatesValid(row + 1, col, matrix.GetLength(0)))
+            {
+                matrix[row + 1, col] = "X";
+            }
+            if (IsCoordinatesValid(row + 1, col + 1, matrix.GetLength(0)))
+            {
+                matrix[row + 1, col + 1] = "X";
+            }
+        }
+
+        private static bool IsCoordinatesValid(int row, int col, int size)
+        {
+            if (row >= size || row < 0 || col >= size || col < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ C# Advanced/EXAMS/C#AdvancedFinalExam20Feb2021/The Race - skeleton/Race.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> racers; 
+        public Race(string name,int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.racers = new List<Racer>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count { get => this.racers.Count; }
+
+        public void Add(Racer Racer) 
+        {
+            if (this.racers.Count<this.Capacity)
+            {
+                this.racers.Add(Racer);
+            }
+        }
+
+        public bool Remove(string name) 
+        {
+            var toRemove = this.racers.FirstOrDefault(r => r.Name == name);
+            return this.racers.Remove(toRemove);
+        }
+
+        public Racer GetOldestRacer() 
+        {
+            return this.racers.OrderByDescending(r => r.Age).First();
+        }
+
+        public Racer GetRacer(string name) 
+        {
+            return this.racers.FirstOrDefault(r => r.Name == name);
+        }
+
+        public Racer GetFastestRacer() 
+        {
+            return this.racers.OrderByDescending(r => r.Car.Speed).First();
+        }
+
+        public string Report() 
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Racers participating at {this.Name}:");
+            foreach (var racer in this.racers)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
