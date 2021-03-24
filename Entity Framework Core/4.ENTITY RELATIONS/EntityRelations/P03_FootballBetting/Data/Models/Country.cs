394dbd5e3af51de00ed1a9af1b2633d9f4a06760
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-03-24 21:57:09 +0200

    Added Entity Relations - Exercise

---
+++ Entity Framework Core/4.ENTITY RELATIONS/EntityRelations/P03_FootballBetting/Data/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    //•	Country – CountryId, Name
+    public class Country
+    {
+        public Country()
+        {
+            this.Towns = new HashSet<Town>();
+        }
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Town> Towns { get; set; }
+    }
+}
---
+++ Entity Framework Core/4.ENTITY RELATIONS/EntityRelations/P03_FootballBetting/Data/Models/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    //•	Team – TeamId, Name, LogoUrl, Initials (JUV, LIV, ARS…), Budget, PrimaryKitColorId, SecondaryKitColorId, TownId
+    public class Team
+    {
+        public Team()
+        {
+            this.Players = new HashSet<Player>();
+            this.HomeGames = new HashSet<Game>();
+            this.AwayGames = new HashSet<Game>();
+        }
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public string LogoUrl { get; set; }
+        public string Initials { get; set; }
+        public decimal Budget { get; set; }
+        public int PrimaryKitColorId { get; set; }
+        public Color PrimaryKitColor { get; set; }
+        public int SecondaryKitColorId { get; set; }
+        public Color SecondaryKitColor { get; set; }
+        public int TownId { get; set; }
+        public Town Town { get; set; }
+
+        public ICollection<Player> Players { get; set; }
+
+        [InverseProperty("HomeTeam")]
+        public ICollection<Game> HomeGames { get; set; }
+        [InverseProperty("AwayTeam")]
+        public ICollection<Game> AwayGames { get; set; }
+    }
+}
