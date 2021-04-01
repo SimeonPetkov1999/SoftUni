2cfa729dc91229841f2bad5c0dcf10a190847879
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-04-01 17:22:23 +0300

    Added Auto Mapping

---
+++ Entity Framework Core/7.C# AUTO MAPPING OBJECTS/fastFood/FastFood.Core/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Employees;
+
+    public class EmployeesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public EmployeesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Register()
+        {
+            var position = this.context.Positions
+               .ProjectTo<RegisterEmployeeViewModel>(mapper.ConfigurationProvider)
+               .ToList();
+
+            return this.View(position);
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterEmployeeInputModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult All()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
