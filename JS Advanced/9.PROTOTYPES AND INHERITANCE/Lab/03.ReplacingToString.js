e3192e5104fdc69a8bf2e29a6ef2b0f516f525ff
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-04-11 20:18:52 +0300

    Added Inheritance JS - Lab

---
+++ JS Advanced/9.PROTOTYPES AND INHERITANCE/Lab/03.ReplacingToString.js
@@ -0,0 +1,46 @@
+function toStringExtension()
+{
+    class Person
+    {
+        constructor(name, email)
+        {
+            this.name = name;
+            this.email = email;
+        }
+
+        toString()
+        {
+            return `${this.constructor.name} (name: ${this.name}, email: ${this.email})`; 
+        }
+    }
+
+    class Teacher extends Person
+    {
+        constructor(name, email, subject)
+        {
+            super(name, email);
+            this.subject = subject;
+        }
+        toString(){
+            return super.toString().substr(0, super.toString().length-1) + `, subject: ${this.subject})`;
+        }
+
+    }
+
+    class Student extends Person
+    {
+        constructor(name, email, course)
+        {
+            super(name, email);
+            this.course = course;
+        }
+
+        toString()
+        {
+            return super.toString() + `, Course:${this.subject})`
+        }
+
+    }
+
+    return { Person, Teacher, Student }
+}
