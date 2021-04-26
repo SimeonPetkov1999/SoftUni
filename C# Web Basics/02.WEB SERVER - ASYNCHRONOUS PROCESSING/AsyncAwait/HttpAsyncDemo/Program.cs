3ef819ac9a8625bcf7b34bd84b3ebb19d45b5341
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2021-04-26 21:02:08 +0300

    Added Async,Await Demo

---
+++ C# Web Basics/02.WEB SERVER - ASYNCHRONOUS PROCESSING/AsyncAwait/HttpAsyncDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAsyncDemo
+{
+    class Program
+    {
+        const string NewLine = "\r\n";
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+         
+            TcpListener tcpListener = new TcpListener(
+                IPAddress.Loopback, 80);
+            tcpListener.Start();
+            while (true)
+            {
+                var client = await tcpListener.AcceptTcpClientAsync();
+                await ProcessClinetAsync(client);
+            }
+        }
+
+        private static async Task ProcessClinetAsync(TcpClient client)
+        {
+            var stream = client.GetStream();
+            byte[] buffer = new byte[1000000];
+            var lenght = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            string requestString =
+                Encoding.UTF8.GetString(buffer, 0, lenght);
+            Console.WriteLine(requestString);
+
+            string html = $"<h1>Hello from SimoServer {DateTime.Now}</h1>";
+
+
+            string response = "HTTP/1.1 200 OK" + NewLine +
+                "Server: DemoServer" + NewLine +
+                "Content-Type: text/html; charset=utf-8" + NewLine +
+                "Content-Lenght: " + html.Length + NewLine +
+                NewLine +
+                html + NewLine;
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseBytes);
+
+            Console.WriteLine(new string('=', 70));
+        }
+    }
+}
