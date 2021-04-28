97994f6ccd181fd973ecc18dd7914a8b28dd1484
Simeon Petkov <simeonpetkov1999@gmail.com>
2021-04-28 21:00:08 +0300

    Created Basic HTTP Functionality

---
+++ C# Web Basics/SIS/SIS/SUSApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using SUS.HTTP;
+using SUS.HTTP.Contracts;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUSApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHttpServer server = new HttpServer();
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/about", About);
+            server.AddRoute("/simo", (HttpRequest request) => 
+            {
+                var responseHtml = $"<h1>Welcome! {DateTime.Now}</h1>";
+                var responseBytes = Encoding.UTF8.GetBytes(responseHtml);
+                var response = new HttpResponse("text/xml", responseBytes);
+                return response;
+            });
+            await server.StartAsync(80);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            var responseHtml = "<h1>Welcome!</h1>" +
+                request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+            var responseHtml = "<h1>About...</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+            var responseHtml = "<h1>Login...</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+    }
+}
+
