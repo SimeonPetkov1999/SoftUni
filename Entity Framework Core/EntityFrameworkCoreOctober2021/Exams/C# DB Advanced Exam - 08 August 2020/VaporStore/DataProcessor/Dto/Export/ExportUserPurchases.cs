c77bc4c8b3f0b9581cfcea209ff381426c93c777
Simeon Petkov <simeonpetkov1999@gmail.com>
2021-12-03 19:08:30 +0200

    Added C# DB Advanced Exam - 08 August 2020

---
+++ Entity Framework Core/EntityFrameworkCoreOctober2021/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Dto/Export/ExportUserPurchases.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.Dto.Export
+{
+    [XmlType("User")]
+    public class ExportUserPurchases
+    {
+        [XmlAttribute("username")]
+        public string Username { get; set; }
+
+        [XmlArray("Purchases")]
+        public PurchaseDto[] Purchases { get; set; }
+
+        public double TotalSpent { get; set; }
+    }
+
+    [XmlType("Purchase")]
+    public class PurchaseDto
+    {
+        public string Card { get; set; }
+
+        public string Cvc { get; set; }
+
+        public string Date { get; set; }
+
+        public GameDto Game { get; set; }
+    }
+
+    [XmlType("Game")]
+    public class GameDto 
+    {
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public double Price { get; set; }
+    }
+}
--- Entity Framework Core/EntityFrameworkCoreOctober2021/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
+++ Entity Framework Core/EntityFrameworkCoreOctober2021/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
@@ -1,44 +1,80 @@
 ﻿namespace VaporStore.DataProcessor
 {
-	using System;
+    using System;
     using System.Linq;
     using Data;
     using Newtonsoft.Json;
+    using VaporStore.Data.Models.Enums;
+    using VaporStore.DataProcessor.Dto.Export;
 
     public static class Serializer
-	{
-		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
-		{
-			var games = context.Genres
-				.Where(x => genreNames.Contains(x.Name))
-				.ToList()
-				.Select(x => new
-				{
-					Id = x.Id,
-					Genre = x.Name,
-					Games = x.Games.Where(x => x.Purchases.Any()).ToList().Select(g => new
-					{
-						Id = g.Id,
-						Title = g.Name,
-						Developer = g.Developer.Name,
-						Tags = string.Join(", ", g.GameTags.Select(x => x.Tag.Name)),
-						Players = g.Purchases.Count
-					}).OrderByDescending(x => x.Players)
-					  .ThenBy(x => x.Id)
-					  .ToList(),
-					TotalPlayers = x.Games.Select(x=>x.Purchases.Count).Sum()
-				}).OrderByDescending(x => x.TotalPlayers)
-				.ThenBy(x => x.Id)
-				.ToList();
+    {
+        public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
+        {
+            var games = context.Genres
+                .Where(x => genreNames.Contains(x.Name))
+                .ToList()
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Genre = x.Name,
+                    Games = x.Games.Where(x => x.Purchases.Any()).ToList().Select(g => new
+                    {
+                        Id = g.Id,
+                        Title = g.Name,
+                        Developer = g.Developer.Name,
+                        Tags = string.Join(", ", g.GameTags.Select(x => x.Tag.Name)),
+                        Players = g.Purchases.Count
+                    }).OrderByDescending(x => x.Players)
+                      .ThenBy(x => x.Id)
+                      .ToList(),
+                    TotalPlayers = x.Games.Select(x => x.Purchases.Count).Sum()
+                }).OrderByDescending(x => x.TotalPlayers)
+                .ThenBy(x => x.Id)
+                .ToList();
 
-			var json = JsonConvert.SerializeObject(games,Formatting.Indented);
+            var json = JsonConvert.SerializeObject(games, Formatting.Indented);
 
-			return json;
-		}
+            return json;
+        }
 
-		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
+        {
+            var typeToCheck = Enum.Parse<PurchaseType>(storeType);
+
+            var users = context.Users
+                .Where(x => x.Cards.Any(x => x.Purchases.Any()))
+                .Select(x => new ExportUserPurchases
+                {
+                    Username = x.Username,
+                    Purchases = x.Cards.SelectMany(x => x.Purchases.Where(x => x.Type == typeToCheck))
+                    .OrderBy(x => x.Date)
+                    .Select(x => new PurchaseDto
+                    {
+                        Card = x.Card.Number,
+                        Cvc = x.Card.Cvc,
+                        Date = x.Date.ToString("yyyy-MM-dd HH:mm"),
+                        Game = new GameDto()
+                        {
+                            Genre = x.Game.Genre.Name,
+                            Price = (double)(x.Game.Price ==0 ? 0 :x.Game.Price),
+                            Title = x.Game.Name,
+                        }
+                    })
+                    .ToArray(),
+                    TotalSpent = (double)x.Cards.SelectMany(x => x.Purchases.Where(x => x.Type == typeToCheck)).Sum(x => x.Game.Price)
+                })
+                 
+                .OrderByDescending(x => x.TotalSpent)
+                .ThenBy(x => x.Username)
+                .ToList()
+                .Where(u => u.Purchases.Count() > 0)
+                .ToList();
+
+
+            var xml = XmlConverter.Serialize(users, "Users");
+
+            return xml;
+        }
+    }
 }
